36587614a6aa738c72e6a4bb02172f298bb2e860
Ben Read <endymion1818@gmail.com>
2019-07-02 12:36:35 +0100

    image and svg modules fix (#11)

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -2,23 +2,23 @@ declare module '.svg' {
   const content: string
   export default content
 }
-declare module '.jpg' {
+declare module '*.jpg' {
   const content: string
   export default content
 }
-declare module '.jpeg' {
+declare module '*.jpeg' {
   const content: string
   export default content
 }
-declare module '.png' {
+declare module '*.png' {
   const content: string
   export default content
 }
-declare module '.woff' {
+declare module '*.woff' {
   const content: string
   export default content
 }
-declare module '.woff2' {
+declare module '*.woff2' {
   const content: string
   export default content
 }
