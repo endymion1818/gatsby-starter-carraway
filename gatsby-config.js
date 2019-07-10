1f59c25ee641339b173db274330ffc7be7d30b1f
Benjamin Read <endymion1818@gmail.com>
2019-07-10 16:09:07 +0100

    404 page, icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `src/assets/icon.png`,
       },
     },
     `gatsby-remark-copy-linked-files`,
